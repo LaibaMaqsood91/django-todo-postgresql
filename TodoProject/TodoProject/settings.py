4793135b9f4a98ea364924c10bdc74609875c32a
LaibaMaqsood91 <alishbahmaqsood09@gmail.com>
2025-10-30 02:09:42 +0500

    Update settings.py
    Added CI/CD pipelines

--- TodoProject/TodoProject/settings.py
+++ TodoProject/TodoProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", ".railway.app"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
